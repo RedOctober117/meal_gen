e47493b42fa178269b924a9d84e18e81f25ec4e8
RedOctober117 <dbcahilly3@gmail.com>
2022-12-17 11:21:38 -0500

    progress on sql build.sql

--- sql/build.sql
+++ sql/build.sql
@@ -1,1 +1,25 @@
-DROP 
+-- This script creates the meal_gen database. It should include all the relevant data. 
+
+DROP DATABASE IF EXISTS meal_gen
+
+CREATE DATABASE IF NOT EXISTS meal_gen
+
+USE meal_gen
+
+CREATE TABLE servings (
+  serv_id         INT          NOT NULL     UNIQUE  AUTO_INCREMENT  PRIMARY KEY,
+  serv_name       VARCHAR(50)  NOT NULL     UNIQUE,
+  serv_size       VARCHAR(5)   NOT NULL,
+  serv_cal        INT          DEFAULT 0, 
+  serv_fat_tot    INT          DEFAULT 0,
+  serv_fat_sat    INT          DEFAULT 0,
+  serv_fat_trans  INT          DEFAULT 0,
+  serv_chol       INT          DEFAULT 0,
+  serv_sod        INT          DEFAULT 0,
+  serv_carb_tot   INT          DEFAULT 0,
+  serv_fiber_diet INT          DEFAULT 0,
+  serv_sugar_tot  INT          DEFAULT 0,
+  serv_sugar_add  INT          DEFAULT 0,
+  serv_prot       INT          DEFAULT 0,
+  
+)
