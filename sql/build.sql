cc38b6210ce616273cd78bae62cbc71957dc7132
RedOctober117 <dbcahilly3@gmail.com>
2022-12-21 11:23:42 -0500

    created servings label table

--- sql/build.sql
+++ sql/build.sql
@@ -6,25 +6,32 @@ CREATE DATABASE IF NOT EXISTS meal_gen;
 
 USE meal_gen;
 
+CREATE TABLE default_serving_units (
+  default_serving_type      VARCHAR(50)       NOT NULL    UNIQUE,
+  default_serving_unit      VARCHAR(50),
+  PRIMARY KEY (default_serving_type)
+);
+
 CREATE TABLE servings (
-  serv_id         INT          NOT NULL     UNIQUE  AUTO_INCREMENT,
-  serv_name       VARCHAR(50)  NOT NULL     UNIQUE,
-  serv_size       VARCHAR(5)   NOT NULL,
-  serv_cal        INT          DEFAULT 0, 
-  serv_fat_tot    INT          DEFAULT 0,
-  serv_fat_sat    INT          DEFAULT 0,
-  serv_fat_trans  INT          DEFAULT 0,
-  serv_chol       INT          DEFAULT 0,
-  serv_sod        INT          DEFAULT 0,
-  serv_carb_tot   INT          DEFAULT 0,
-  serv_fiber_diet INT          DEFAULT 0,
-  serv_sugar_tot  INT          DEFAULT 0,
-  serv_sugar_add  INT          DEFAULT 0,
-  serv_prot       INT          DEFAULT 0,
-  serv_vit_d      INT          DEFAULT 0,
-  serv_calc       INT          DEFAULT 0,
-  serv_iron       INT          DEFAULT 0,
-  serv_potas      INT          DEFAULT 0,
+  serv_id         INT            NOT NULL     UNIQUE      AUTO_INCREMENT,
+  serv_name       VARCHAR(50)    NOT NULL     UNIQUE,
+  serv_size       DECIMAL(5,2)   NOT NULL,
+  serv_unit       VARCHAR(50)    NOT NULL,
+  serv_cal        INT            DEFAULT 0, 
+  serv_fat_tot    INT            DEFAULT 0,
+  serv_fat_sat    INT            DEFAULT 0,
+  serv_fat_trans  INT            DEFAULT 0,
+  serv_chol       INT            DEFAULT 0,
+  serv_sod        INT            DEFAULT 0,
+  serv_carb_tot   INT            DEFAULT 0,
+  serv_fiber_diet INT            DEFAULT 0,
+  serv_sugar_tot  INT            DEFAULT 0,
+  serv_sugar_add  INT            DEFAULT 0,
+  serv_prot       INT            DEFAULT 0,
+  serv_vit_d      INT            DEFAULT 0,
+  serv_calc       INT            DEFAULT 0,
+  serv_iron       INT            DEFAULT 0,
+  serv_potas      INT            DEFAULT 0,
   PRIMARY KEY (serv_id, serv_name)
 );
 
@@ -50,4 +57,25 @@ CREATE TABLE meal_lun (
 
 CREATE TABLE meal_din (
   meal_din_id
-)
+);
+
+INSERT INTO default_serving_units (default_serving_type, default_serving_unit) VALUES
+  ("Calories", NULL),
+  ("Total Fat", "g"),
+  ("Saturated Fat", "g"),
+  ("Trans Fat", "g"),
+  ("Cholesterol", "mg"),
+  ("Sodium", "mg"),
+  ("Total Carbohydrates", "g"),
+  ("Dietary Fiber", "g"),
+  ("Total Sugars", "g"),
+  ("Added Sugars", "g"),
+  ("Protein", "g"),
+  ("Vitamin D", "mcg"),
+  ("Calcium", "mg"),
+  ("Iron", "mg"),
+  ("Potassium", "mg");
+
+
+
+
